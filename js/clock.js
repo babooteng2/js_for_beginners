114ea8bff56daa7730166054fcf92ddcf394860d
Tom Kim <babooteng2@gmail.com>
2021-07-26 01:44:05 +0900

    #4.1 Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -4,6 +4,16 @@ function sayHello() {
   console.log("hello");
 }
 
-setInterval(() => {
+setTimeout(() => {
   sayHello();
-}, 100000);
+}, 3000);
+
+function getTime() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+getTime();
+setInterval(() => {
+  getTime();
+}, 1000);
