e3846d2c59e1f7093bfbceb6b4ef6fc42c1e6622
Tom Kim <babooteng2@gmail.com>
2021-07-27 11:22:11 +0900

    #7.1 Adding To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,19 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
+function paintToDo(newToDo) {
+  const nodeLi = document.createElement("li");
+  const nodeSpan = document.createElement("span");
+  nodeSpan.innerText = newToDo;
+  nodeLi.appendChild(nodeSpan);
+  todoList.appendChild(nodeLi);
+}
+
 function hdTodoSubmit(e) {
   e.preventDefault();
-  const nodeLi = document.createElement("li");
-  nodeLi.innerText = todoInput.value;
-  todoList.appendChild(nodeLi);
+  const newToDo = todoInput.value;
   todoInput.value = "";
+  paintToDo(newToDo);
 }
 
 todoForm.addEventListener("submit", hdTodoSubmit);
