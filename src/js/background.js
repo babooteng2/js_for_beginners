968eb78915dd69ee45439a2eb74aef685225505b
Tom Kim <babooteng2@gmail.com>
2021-08-09 05:03:34 +0900

    background.js style modify

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,5 @@
 const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 const getRandImg = images[Math.floor(Math.random() * images.length)];
-const bgImg = document.createElement("img");
-bgImg.src = `img/${getRandImg}`;
-document.body.appendChild(bgImg);
+const bgsrc = `../img/${getRandImg}`;
+const body = document.querySelector("body");
+body.style.backgroundImage = `url(${bgsrc})`;
