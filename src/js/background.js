fb1f18f0271c693d23392a5f307bba03653af83a
Tom Kim <babooteng2@gmail.com>
2021-08-09 07:18:23 +0900

    background target change

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,5 @@
 const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 const getRandImg = images[Math.floor(Math.random() * images.length)];
-const bgsrc = `../img/${getRandImg}`;
+const bgsrc = `./img/${getRandImg}`;
 const body = document.querySelector("body");
 body.style.backgroundImage = `url(${bgsrc})`;
